2c0cad9ccda3b91610e818f2d6b1bf25f93018b2
Home User <home@jacobs-mbp.home>
2016-05-25 18:58:42 +1000

    Implement alternative voting counting method

--- fairvote/client/lib/voting.js
+++ fairvote/client/lib/voting.js
@@ -1,3 +1,112 @@
+getWinnerAV = function () {
+  var currentPoll = Session.get("currentPoll");
+  var winner = false;
+  var tie = false;
+  var winningChoice;
+  // var FAKEVOTEDATA = [];
+  var votes = currentPoll.votes;
+
+  // Repeat alternative vote counting method until winner is found
+  while(!winner && !tie) {
+    // Count total first preference votes
+    totalFirstVotes = 0;
+    votes.forEach( function (vote) {
+      if ((vote.choice != "") && (vote.preference == "1")) {
+        totalFirstVotes += 1;
+      }
+    });
+
+    var choices = Session.get("currentChoices");
+    // var choices = ["A", "B", "C", "D"];
+    var countedVotes = countVotes(choices, votes);
+
+    // Find choice that has maximum first preference votes
+    var maxFirstVotes = 0;
+    var maxFirstVotesChoice;
+    countedVotes.forEach( function (countedVote) {
+      if (countedVote.numVotes > maxFirstVotes) {
+        maxFirstVotes = countedVote.numVotes;
+        maxFirstVotesChoice = countedVote.choice;
+      }
+    });
+    // Check if we have a winner (a choice has over 50% of first preference votes)
+    if (maxFirstVotes > (totalFirstVotes/2)) {
+      // We have a winner
+      winner = true;
+      winningChoice = maxFirstVotesChoice;
+    } 
+
+    // Check if we don't have a winner
+    if (!winner) {
+      // Find losing choice (one with least first preference votes)
+      var minFirstVotes = maxFirstVotes;
+      tie = true;
+      countedVotes.forEach( function (countedVote) {
+        if ((countedVote.numVotes < minFirstVotes) && (countedVote.numVotes != 0)) {
+          minFirstVotes = countedVote.numVotes;
+          tie = false;
+        }
+      });
+
+      // Get all losing choices (there may be more than one)
+      var minFirstVotesChoices = [];
+      countedVotes.forEach( function (countedVote) {
+        if (countedVote.numVotes == minFirstVotes) {
+          minFirstVotesChoices.push(countedVote);
+        }
+      });
+
+      // Eliminate losing choice/s and transfer votes to second preference
+      var senderAddresses = [];
+      minFirstVotesChoices.forEach( function (minFirstVotesChoice) {
+        votes.forEach( function (vote) {
+          if ((vote.choice == minFirstVotesChoice.choice) && (vote.preference == "1")) {
+            // Replace eliminated choice/s first preference with empty string
+            vote.preference = "";
+            // Store sender addresses for users that voted for eliminated choice/s
+            senderAddresses.push(vote.sender);
+          }
+        });
+      });
+
+      // For all users that voted for eliminated choice, shift other preferences on ballot down one
+      votes.forEach( function (vote) {
+        // Check if vote belongs to user that voted for eliminated choice and vote preference is not empty
+        if (($.inArray(vote.sender, senderAddresses) > -1) && (vote.preference != "")) {
+          var preference = parseInt(vote.preference);
+          preference -= 1;
+          vote.preference = preference.toString();
+        }
+      });
+    }
+  }
+
+  // Check that there was not a tie
+  if (!tie) {
+    // Return winning choice
+    console.log("WINNER IS: " + winningChoice);
+    return winningChoice;
+  }
+  // Return false if there is a tie
+  console.log("TIE! THERE IS NO WINNER");
+  return false;
+}
+
+getWinnerMaxVotes = function() {
+  var countedVotes = Session.get("countedVotes");
+  var maxVotes = 0;
+  var winningChoice = false;
+  if (countedVotes){
+    countedVotes.forEach( function (countedVote) {
+      if (countedVote.numVotes > maxVotes) {
+        maxVotes = countedVote.numVotes;
+        winningChoice = countedVote.choice;
+      }
+    });
+  }
+  return winningChoice;
+}
+
 Template.vote.helpers({
   choices() {
   	return Session.get("currentChoices");
@@ -114,20 +223,19 @@ Template.viewvotes.helpers({
     }
   },
   winner() {
-    var countedVotes = Session.get("countedVotes");
-    var maxVotes = 0;
-    var winner = false;
-    if (countedVotes){
-      countedVotes.forEach( function (countedVote) {
-        if (countedVote.numVotes == maxVotes) {
-          winner = false;
-        }
-        if (countedVote.numVotes > maxVotes) {
-          maxVotes = countedVote.numVotes;
-          winner = countedVote.choice;
-        }
-      });
+    var currentPoll = Session.get("currentPoll");
+    if (!currentPoll) return;
+
+    // Get winner for poll
+    var winner;
+    if (currentPoll.pollType == "AV") {
+      // Calculate winner using alternative vote methodology
+      winner = getWinnerAV();
+    } else {
+      // Calculate winner from number of max votes
+      winner = getWinnerMaxVotes();
     }
+
     return winner;
   },
 });
